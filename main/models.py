8903781b7d1f416f2f26adfb348fa75c0fc6aa47
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2024-11-24 16:43:10 +0500

    models

--- main/models.py
+++ main/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class UserMod(models.Model):
-    pass
+    user_id = models.PositiveBigIntegerField(verbose_name='telegram id')
+    user_name = models.CharField(verbose_name='username', max_length=100)
+    full_name = models.CharField(verbose_name='full name', max_length=150)
 
     def __str__(self):
-        return ''
+        return self.full_name
 
     class Meta:
-        verbose_name = ''
-        verbose_name_plural = ''
+        verbose_name = 'Foydalanuvchi'
+        verbose_name_plural = 'Foydalanuvchilar'
