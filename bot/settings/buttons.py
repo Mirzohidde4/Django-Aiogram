89a5035acacb05a36465a4e28f5f04dd026d9b28
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2025-01-19 13:59:15 +0500

    buttons

--- bot/settings/buttons.py
+++ bot/settings/buttons.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup, KeyboardBut
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 
-def CreateInline(*button_rows, just=int) -> InlineKeyboardMarkup: #! {a: a, b: b}
+def CreateInline(*button_rows, just=(1,)) -> InlineKeyboardMarkup: #! {a: a, b: b}
     builder = InlineKeyboardBuilder()
     for row in button_rows:
         for text, callback_data in row.items():
@@ -10,14 +10,14 @@ def CreateInline(*button_rows, just=int) -> InlineKeyboardMarkup: #! {a: a, b: b
                 builder.add(InlineKeyboardButton(text=text,url=callback_data))
             else:
                 builder.add(InlineKeyboardButton(text=text, callback_data=callback_data))
-    builder.adjust(just)
+    builder.adjust(*just)
     return builder.as_markup()
 
 
-def Createreply(*args, contact=False, just=int) -> ReplyKeyboardBuilder: #! 'a', 'b', ..
+def Createreply(*args, contact=False, just=(2,)) -> ReplyKeyboardBuilder: #! 'a', 'b', ..
     bulder = ReplyKeyboardBuilder()
     for i in args:
         bulder.add(KeyboardButton(text=i, request_contact=True if contact else False))
-    bulder.adjust(just)
+    bulder.adjust(*just)
     return bulder.as_markup(resize_keyboard=True, one_time_keyboard=True)
         
